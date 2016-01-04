e689d13072d84e31ef7033f67e892fb0ac5cc6c2
Julian Wise - WLAPTOP <Julianadamwise@gmail.com>
2016-01-04 18:25:46 +1100

    Initial_Commit

---
+++ Blog_Julian/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Blog_Julian.Startup))]
+namespace Blog_Julian
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
